56317762c09e6d351f8c8c7eaaf226563cab74ac
pangwa <pangwa@gmail.com>
2023-03-16 12:58:25 +0800

    git credential implementation

--- Program.fs
+++ Program.fs
@@ -1,18 +1,25 @@
-﻿open Fli;
-open Spectre.Console;
+﻿open Spectre.Console
+open Medallion.Shell
+open System
 
-type Colors = Yellow | Green | Blue
+type Colors =
+    | Yellow
+    | Green
+    | Blue
 
+let runGit cmd args =
+    let cmd = Command.Run("git", args |> Seq.toArray)
+    cmd.StandardInput.PipeFromAsync(Console.OpenStandardInput()) |> ignore
+    cmd.StandardOutput.PipeToAsync(Console.OpenStandardOutput()) |> ignore
+    cmd.StandardError.PipeToAsync(Console.OpenStandardError()) |> ignore
+    cmd.Wait()
 
 let gitCredentials url =
-  cli {
-      Exec "git"
-      Arguments [url;]
-  } |> Command.execute
+    AnsiConsole.MarkupLine($"fill credential for [underline blue]{url}[/]")
+    runGit "git" [ "credential"; "fill" ]
 
+let main () =
+    AnsiConsole.MarkupLine("[underline red]Hello[/] git-gr!")
+    gitCredentials "https://test.com" |> System.Console.WriteLine
 
-let main() =
-    AnsiConsole.MarkupLine("[underline red]Hello[/] git-gr!");
-
-
-main()
+main ()
